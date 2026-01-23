64d8bb31534eccdb851d18c68756edf75cde8414
Benjamin Mahootchi <benmahoo2@gmail.com>
2026-01-23 17:28:13 +0100

    Added: Add random rock paper scissor game

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3)
+
+    if (randomNumber <= 0) {
+        return "Rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "Scissors"
+    }
+
+}
+
+console.log(getComputerChoice());
