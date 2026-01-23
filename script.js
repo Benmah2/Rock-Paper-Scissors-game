10d4e6a876dcc7f879332ced8d7823ea566c2e26
Benjamin Mahootchi <benmahoo2@gmail.com>
2026-01-23 14:34:26 +0100

    Playing around with Javascript

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+function getComputerChoice(test) {
+    return Math.floor(Math.random() * test)
+}
+
+console.log(getComputerChoice(3));
+console.log(getComputerChoice(1));
+console.log(Math.random());
+console.log(Math.random(25));
